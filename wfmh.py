1ff58d50c2d0347a2f8f3164c62d84b720a8416b
Paul Harrington <hello@loosenthedark.tech>
2022-02-05 23:07:14 +0000

    Add additional placeholder route

--- wfmh.py
+++ wfmh.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def landing_page():
     return 'Welcome to Work From My Home!'
+
+
+@app.route("/about")
+def about_wfmh():
+    return 'Work From My Home information page'
