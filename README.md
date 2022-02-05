0e445bc98aa83207cfe7368b11c9cfed6680c8d3
Paul Harrington <hello@loosenthedark.tech>
2022-02-05 23:12:58 +0000

    Implement & test dynamic profile page route

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 https://flask.palletsprojects.com/en/2.0.x/quickstart/
 
+Dynamic routes: https://pythonise.com/series/learning-flask/generating-dynamic-urls-with-flask
--- wfmh.py
+++ wfmh.py
@@ -8,6 +8,11 @@ def landing_page():
     return 'Welcome to Work From My Home!'
 
 
-@app.route("/about")
+@app.route("/about/<username>")
 def about_wfmh():
     return 'Work From My Home information page'
+
+
+@app.route("/profile/<username>")
+def profile_page(username):
+    return f"This is {username}'s Work From My Home profile page"
