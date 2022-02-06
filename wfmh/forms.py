ad6f5f9d04d1d77d7c84a0e60226f8e6421c0a55
Paul Harrington <hello@loosenthedark.tech>
2022-02-06 22:39:32 +0000

    Strengthen form validation

--- wfmh/forms.py
+++ wfmh/forms.py
@@ -1,23 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
+from wfmh.models import Worker
 
 
 class WFMHRegistrationForm(FlaskForm):
 
-    # def validate_username(self, username_to_check):
-    #     user_already_registered = User.query.filter_by(
-    #         username=username_to_check.data).first()
-    #     if user_already_registered:
-    #         raise ValidationError(
-    #             'That username is already registered! Please try again with a different username.')
+    def validate_r_profile_name(self, new_user):
+        previously_registered_user = Worker.query.filter_by(
+            profile_name=new_user.data).first()
+        if previously_registered_user:
+            raise ValidationError(
+                'That username is already registered! Please try again with a different username.')
 
-    # def validate_email_address(self, email_address_to_check):
-    #     email_already_registered = User.query.filter_by(
-    #         email_address=email_address_to_check.data).first()
-    #     if email_already_registered:
-    #         raise ValidationError(
-    #             'That email is already registered! Please try again with a different email address.')
+    def validate_r_email(self, new_email):
+        previously_registered_email = Worker.query.filter_by(
+            worker_email=new_email.data).first()
+        if previously_registered_email:
+            raise ValidationError(
+                'That email is already registered! Please try again with a different email address.')
 
     r_profile_name = StringField(label='Enter profile name', validators=[Length(min=3, max=20), DataRequired()])
     r_email = StringField(label='Enter email', validators=[Length(min=7), Email(), DataRequired()])
