2d1b7355c21fc205b3c546e3a5eb1f084d4dd8e7
Paul Harrington <hello@loosenthedark.tech>
2022-02-07 16:52:20 +0000

    Build out login form

--- wfmh/forms.py
+++ wfmh/forms.py
@@ -28,3 +28,25 @@ class WFMHRegistrationForm(FlaskForm):
     r_password_confirm = PasswordField(
         label='Confirm password', validators=[EqualTo('r_password'), DataRequired()])
     r_submit = SubmitField(label='REGISTER')
+
+
+class WFMHLoginForm(FlaskForm):
+
+    # def validate_r_profile_name(self, new_user):
+    #     previously_registered_user = Worker.query.filter_by(
+    #         profile_name=new_user.data).first()
+    #     if previously_registered_user:
+    #         raise ValidationError(
+    #             'That username is already registered! Please try again with a different username.')
+
+    # def validate_r_email(self, new_email):
+    #     previously_registered_email = Worker.query.filter_by(
+    #         worker_email=new_email.data).first()
+    #     if previously_registered_email:
+    #         raise ValidationError(
+    #             'That email is already registered! Please try again with a different email address.')
+
+    l_profile_name = StringField(label='Enter profile name', validators=[DataRequired()])
+    l_password = PasswordField(
+        label='Enter password', validators=[DataRequired()])
+    l_submit = SubmitField(label='LOGIN')
--- wfmh/routes.py
+++ wfmh/routes.py
@@ -1,6 +1,6 @@
 from flask import flash, redirect, render_template, url_for
 from wfmh import app, db
-from wfmh.forms import WFMHRegistrationForm
+from wfmh.forms import WFMHLoginForm, WFMHRegistrationForm
 from wfmh.models import Home, Worker
 
 @app.route("/")
@@ -39,3 +39,21 @@ def wfmh_registration():
             flash(f'There was a problem with your registration attempt: {error_message}', category='danger')
 
     return render_template('register.html', form=r_form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def wfmh_login():
+    l_form = WFMHLoginForm()
+    # if r_form.validate_on_submit():
+    #     successfully_registered_user = Worker(profile_name=r_form.r_profile_name.data,
+    #                                           worker_email=r_form.r_email.data, make_password_secure=r_form.r_password.data)
+    #     db.session.add(successfully_registered_user)
+    #     db.session.commit()
+    #     return redirect(url_for('browse_homes'))
+
+    # if r_form.errors != {}:
+    #     for error_message in r_form.errors.values():
+    #         flash(
+    #             f'There was a problem with your registration attempt: {error_message}', category='danger')
+
+    return render_template('login.html', form=l_form)
