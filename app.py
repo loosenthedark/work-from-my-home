42368aff94329c932504b37779d511740de33f4a
Paul Harrington <hello@loosenthedark.tech>
2022-02-08 17:39:15 +0000

    Create & configure Procfile prior to deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from wfmh import app
 
-# Checks if the run.py file has executed directly and is not being imported
+# Checks if the app.py file has executed directly and is not being imported
 if __name__ == '__main__':
     app.run(debug=True)
