73dd86d6f457f2ede992175f6fb333588682b3bb
Paul Harrington <hello@loosenthedark.tech>
2022-02-08 21:03:20 +0000

    Reattempt Heroku deployment - this time from the CLI

--- wfmh/__init__.py
+++ wfmh/__init__.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         'SQLALCHEMY_DATABASE_URI')
-app.secret_key = os.environ.get(
+app.config[
+    'SECRET_KEY'] = os.environ.get(
         'SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
