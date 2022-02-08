e29cf72585946831327ed2785a6915ce67119730
Paul Harrington <hello@loosenthedark.tech>
2022-02-08 20:14:03 +0000

    Modify secret_key configuration to try to resolve deployment failure

--- wfmh/__init__.py
+++ wfmh/__init__.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         'SQLALCHEMY_DATABASE_URI')
-app.config[
-    'SECRET_KEY'] = os.environ.get(
+app.secret_key = os.environ.get(
         'SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
