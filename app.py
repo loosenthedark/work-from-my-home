d4f51483dd5d31627cb16e17629de2f35acac8cc
Paul Harrington <hello@loosenthedark.tech>
2022-02-08 22:47:39 +0000

    Enable debug mode

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from wfmh import app
 # Checks if the app.py file has executed directly and is not being imported
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")), debug=0)
+        port=int(os.environ.get("PORT", "5000")), debug=1)
