d7218ac17e65ea327d4bd8d76ef8cf860d99fb3c
Paul Harrington <hello@loosenthedark.tech>
2022-02-08 22:28:45 +0000

    Add DATABASE_CONNECTION configuration to __init__.py file

--- wfmh/__init__.py
+++ wfmh/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from sqlalchemy import create_engine
+from wfmh import routes
 if os.path.exists('env.py'):
     import env
 
@@ -11,17 +13,17 @@ app = Flask(__name__)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         'SQLALCHEMY_DATABASE_URI')
-app.config[
-    'SECRET_KEY'] = os.environ.get(
-        'SECRET_KEY')
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+DATABASE_CONNECTION = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI')
+engine = create_engine(DATABASE_CONNECTION, pool_size=50, max_overflow=100)
 
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'wfmh_login'
 login_manager.login_message_category = 'warning'
-
-from wfmh import routes
